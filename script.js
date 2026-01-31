e02f6f529783c79d5da006d3d1067eb6a8e676a0
chareeselam <chareeselam@gmail.com>
2026-01-31 17:02:02 -0500

    fixed close menu button

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const hamMenu = document.querySelector('.ham-menu');
-
+const hamMenu = document.querySelector('#ham-menu');
 const offScreenMenu = document.querySelector('#off-screen-menu');
+const closeMenu = document.querySelector('#close-menu');
 
-hamMenu.addEventListener('click',() => {
+hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
-})
+});
+
+closeMenu.addEventListener('click', () => {
+    hamMenu.classList.remove('active');
+    offScreenMenu.classList.remove('active');
+});
