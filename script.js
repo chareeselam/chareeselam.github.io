178af24d9515536b9dcd0dd663d35c142556c382
chareeselam <chareeselam@gmail.com>
2026-05-14 01:46:01 -0400

    on load animation fix

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const hamMenu = document.querySelector('#ham-menu');
-const offScreenMenu = document.querySelector('#off-screen-menu');
-const closeMenu = document.querySelector('#close-menu');
-
-hamMenu.addEventListener('click', () => {
-    // hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-});
-
-closeMenu.addEventListener('click', () => {
-    // hamMenu.classList.remove('active');
-    offScreenMenu.classList.remove('active');
-});
+setTimeout(() => {
+	document.body.classList.add('intro-animation');
+}, 3500);
